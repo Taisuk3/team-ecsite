b78a88861a3456623b09d83afb36b3df99e99aaa
Taisuk3 <seishun17www@gmail.com>
2021-04-27 14:44:36 +0900

    チーム開発

---
+++ DestinationForm.java
@@ -0,0 +1,54 @@
+package jp.co.internous.sugar.model.form;
+
+import java.io.Serializable;
+
+public class DestinationForm implements Serializable {
+
+	private static final long serialVersionUID = 6608875550276432626L;
+	
+	private int userId;
+	private String familyName;
+	private String firstName;
+	private String address;
+	private String telNumber;
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public String getFamilyName() {
+		return familyName;
+	}
+	
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getTelNumber() {
+		return telNumber;
+	}
+	
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+}
